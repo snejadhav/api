352f2de324ed5426872fad64ef5e3900deec4089
Snehal Jadhav <31817193+snejadhav@users.noreply.github.com>
2019-01-29 14:32:11 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,15 @@
+
+
+<?php
+$host='localhost';
+$username='root';
+$pwd='';
+//$db='hospital_db';
+
+if($db=mysqli_connect($host,$username,$pwd,'hospital_db'))
+{
+	//echo "database connected";
+}
+
+
+?>
